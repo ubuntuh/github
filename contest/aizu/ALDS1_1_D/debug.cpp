827dba23479640552674efe9e87bb7092df08596
ubuntuh <ubuntuhaskell@gmail.com>
2015-12-25 23:32:10 +0900

    msg

---
+++ contest/aizu/ALDS1_1_D/debug.cpp
@@ -0,0 +1,30 @@
+#include <cstdarg>
+#include <cstdio>
+#include <string>
+
+namespace {
+    bool isDebug = false;
+    int p(const char *fmt, ...) {
+        va_list args;
+        int result;
+        if (isDebug) {
+            va_start(args, fmt);
+            result = vprintf(fmt, args);
+            va_end(args);
+            return result;
+        } else {
+            return 0;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    int i;
+    for (i = 1; i < argc; i++) {
+        if (std::string(argv[i]) == "--debug") {
+            isDebug = true;
+            p("Debug Mode (isDebug == %d)\n", isDebug);
+            break;
+        }
+    }
+}
